23f1558bec858f61d24bf134aaf069d7cc2567d4
pavan <pavankumarseepana1210@gmail.com>
2025-04-09 23:47:25 +0530

    feat: fixing errors

--- client/src/utils/dateFormat.js
+++ client/src/utils/dateFormat.js
@@ -1,9 +1,9 @@
 export const formatDate = (date) => {
     const options = {
-        year: "2-digit",
-        month: "2-digit",
-        day: "2-digit",
-        allowedHosts: ["localhost:3000"]
-    }
-    return (new Date(date)).toLocaleDateString("en-US", options);
-} 
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit",
+    };
+    return new Date(date).toLocaleDateString("en-US", options);
+  };
+  
