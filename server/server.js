f472c7915992eb288a753fbe9cee9c90e6f5976c
pavan <pavankumarseepana1210@gmail.com>
2025-04-09 22:45:17 +0530

    fix: resolve server path

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ app.use(cors());
 //   app.use(express.static(path.join(__dirname, "../client/build")));
 // }
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(routes);
 
